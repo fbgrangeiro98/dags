a82f0310645c86fb2f66b8111d7aad962ec4671e
fbgrangeiro98 <168757702+fbgrangeiro98@users.noreply.github.com>
2024-06-20 17:13:44 -0300

    teste

---
+++ dag_ingestao_dados_olist.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.now(),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=60),
+}
+
+dag = DAG(
+    'DAG_INGESTAO_DADOS_OLIST',
+    default_args=default_args,
+    description='Descricao do DAG',
+    schedule_interval='0 */1 * * *',
+)
+
+coleta_dados = BashOperator(
+    task_id='coleta_dados',
+    bash_command='python3 /usr/local/airflow/scripts/ingestao_dados_olist.py',
+    dag=dag,
+)
+
+coleta_dados
